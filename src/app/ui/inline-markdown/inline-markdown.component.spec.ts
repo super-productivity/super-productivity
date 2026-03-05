4356404c1f52dd8d63090dd2bfc8c19eba7b0f08
Trang Le <lethutrang101@gmail.com>
2026-03-05 14:59:19 +0700

    fix redundant test

--- src/app/ui/inline-markdown/inline-markdown.component.spec.ts
+++ src/app/ui/inline-markdown/inline-markdown.component.spec.ts
@@ -94,15 +94,15 @@ describe('InlineMarkdownComponent', () => {
     });
 
     it('should insert ** at cursor and pace cursor between pairs of ** when pressing Ctrl + B with no selection', () => {
-    it('should insert _ at cursor and pace cursor between pairs of _ when pressing Ctrl + I with no selection', () => {
+      mockTextareaEl.nativeElement.selectionStart = 5;
       mockTextareaEl.nativeElement.selectionEnd = 5;
       const ev = new KeyboardEvent('keydown', { key: 'b', ctrlKey: true });
       component.keypressHandler(ev);
       expect(mockTextareaEl.nativeElement.value).toBe('Hello**** world');
       expect(mockTextareaEl.nativeElement.setSelectionRange).toHaveBeenCalledWith(7, 7);
-    it('should insert _ at cursor and pace cursor between pairs of _ when pressing Ctrl + I with no selection', () => {
+    });
 
-    fit('should insert _ at cursor and pace cursor between pairs of _ when pressing Ctrl + I with no selection', () => {
+    it('should insert _ at cursor and pace cursor between pairs of _ when pressing Ctrl + I with no selection', () => {
       mockTextareaEl.nativeElement.selectionStart = 5;
       mockTextareaEl.nativeElement.selectionEnd = 5;
       const ev = new KeyboardEvent('keydown', { key: 'i', ctrlKey: true });
