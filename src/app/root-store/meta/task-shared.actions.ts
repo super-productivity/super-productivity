6a7254a5cdada69180703a6df62b3c2797ad7118
hajiboy95 <89179997+hajiboy95@users.noreply.github.com>
2026-06-03 13:00:40 +0200

    fix(tasks): auto-plan tasks when deadline is set to today via short syntax

--- src/app/root-store/meta/task-shared.actions.ts
+++ src/app/root-store/meta/task-shared.actions.ts
@@ -423,6 +423,8 @@ export const TaskSharedActions = createActionGroup({
         remindAt?: number | null;
         isMoveToBacklog?: boolean;
       };
+      autoPlanToday?: string;
+      autoPlanStartOfNextDayDiffMs?: number;
     }) => ({
       ...props,
       meta: {
