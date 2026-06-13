9318367d30594a1c7686988967d2400beb95b335
yontank <yontank007@gmail.com>
2026-06-13 19:38:21 +0300

    refactor(eml-drop): drop empty emails with a snack instead of throwing

--- src/app/core/drop-paste-input/eml-drop.service.ts
+++ src/app/core/drop-paste-input/eml-drop.service.ts
@@ -3,6 +3,7 @@ import { TaskService } from 'src/app/features/tasks/task.service';
 import { SnackService } from '../snack/snack.service';
 import { Log } from '../log';
 import { parseEml } from 'src/app/util/eml-parser';
+import { T } from 'src/app/t.const';
 
 @Injectable({
   providedIn: 'root',
@@ -16,17 +17,20 @@ export class EmlDropService {
       const data = await parseEml(file);
 
       const sender = data.from?.name || data.from?.address || '';
-      const subject = data?.subject || '';
+      const subject = data.subject || '';
 
       // If both are empty, no point in making an empty task.
-      if (!sender && !subject) throw new Error('sender and subject are both empty');
+      if (!sender && !subject) {
+        this._snackService.open({ type: 'WARNING', msg: T.MH.EML_EMPTY });
+        return;
+      }
 
       const message = `${sender}: ${subject}`;
       this.taskService.add(message);
       // TODO: add attachment to task
     } catch (e) {
       Log.err(e);
-      this._snackService.open("Couldn't create task");
+      this._snackService.open({ type: 'ERROR', msg: T.MH.EML_PARSE_ERROR });
     }
     return;
   }
